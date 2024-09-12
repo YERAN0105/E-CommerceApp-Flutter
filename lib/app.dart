ef81976d3922618ffede0e3668eae71121a3827b
YERAN0105 <yeranfernando2002@gmail.com>
2024-09-12 11:59:25 +0530

    added the dart files for constants and created the variables for sizes.dart

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:full_ecom_app/utils/theme/theme.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.system,
+      theme: EAppTheme.lightTheme,
+      darkTheme: EAppTheme.darkTheme,
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:full_ecom_app/utils/constants/colors.dart';
 import 'package:full_ecom_app/utils/theme/theme.dart';
 
+import 'app.dart';
+
 void main() {
+  // Todo: Add Widgets Bindings
+  // Todo: Init Local Storage
+  // Todo: Await Native Splash
+  // Todo: Initialize Firebase
+  // Todo: Initialize Authentication
+
+
+
   runApp(const App());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: EAppTheme.lightTheme,
-      darkTheme: EAppTheme.darkTheme,
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
